17addcee79c10e3c5dfc7a539706473f67446fa5
ralphribeiro <ralph_ribeiro@msn.com>
2021-08-10 17:00:57 -0300

    regras globais

--- src/app/model_copy.py
+++ src/app/model_copy.py
@@ -1,31 +1,63 @@
 from dataclasses import dataclass, field
+from datetime import date
+from typing import Callable
 
 
 @dataclass
 class Papel:
     raw: dict
     nome: str = field(init=False, default_factory=str)
-    items: dict = field(init=False, default_factory=dict)
+    itens: dict = field(init=False, default_factory=dict)
 
     def _sanitiza(self):
-        ch, vl = self.raw.values()
+        ch, vl = self.raw.popitem()
         atributos = {}
-        for li in vl:
+        for li in vl.splitlines():
             chave, valor = li.split(':')
             if '#' in chave:
                 chave = chave.removesuffix('#')
             atributos[chave.lower()] = valor.strip()
-        # self.nome = ch
-        self.items = atributos
+        self.nome = ch
+        self.itens = atributos
 
     def __post_init__(self):
         self._sanitiza()
 
 
-class Regra:
-    ...
-
-
 class Entrante:
-    def __init__(self, papeis: list[Papel], regras: list[Regra]) -> None:
-        self.papeis = papeis
+    def __init__(
+        self, papeis: dict, regras: list[Callable]
+    ) -> None:
+        self.papeis: list[Papel] = self.constroi_papeis(papeis)
+        self.regras = list(regras)
+        self.status = []
+
+    @staticmethod
+    def constroi_papeis(papeis: dict) -> list[Papel]:
+        return [Papel({k: v}) for k, v in papeis.items()]
+
+    def submete_papeis_regras(self):
+        return [fn(self) for fn in self.regras]
+
+
+def regra_consistência_documentos(entrante: Entrante):
+    papeis_ = {}
+    for papel in entrante.papeis:
+        for k, v in papel.itens.items():
+            if k == 'exp':
+                continue
+            if k in papeis_ and papeis_[k][0] != v:
+                if k == 'id':
+                    entrante.status.append('Detainment: ID number mismatch.')
+                else:
+                    entrante.status.append(f'Detainment: {k} mismatch.')
+            papeis_[k] = v, papel.nome
+
+
+def regra_valida_data(entrante: Entrante):
+    for papel in entrante.papeis:
+        a, m, d = (int(p) for p in papel.itens['exp'].split('.'))
+        validade_papel = date(year=a, month=m, day=d)
+        validade = date.fromisoformat('1982-11-22')
+        if validade_papel < validade:
+            entrante.status.append(f'Entry denied: {papel.nome} expired.')
--- tests/test_model_copy.py
+++ tests/test_model_copy.py
@@ -1,10 +1,28 @@
-from src.app.model_copy import Papel
+from src.app.model_copy import (
+    Entrante, Papel,
+    regra_consistência_documentos,
+    regra_valida_data
+)
 
 
 class TestEntrante:
-    def test_papeis_sanitizatos_dado_raw(self):
-        # assert entrante.papeis == 1
-        ...
+    def test_papel_adicionado_dado_raw(self):
+        josef = {"passport": 'ID#: GC07D-FU8AR\nNATION: Arstotzka\nNAME: Costanza, Josef\nDOB: 1933.11.28\nSEX: M\nISS: East Grestin\nEXP: 1983.03.15'}
+        entrante = Entrante(josef, [lambda: None])
+        passaporte = Papel(josef)
+        assert passaporte in entrante.papeis
+        assert entrante.papeis[0].nome == 'passport'
+        assert entrante.papeis[0].itens['id'] == 'GC07D-FU8AR'
+
+    def test_papeis_adicionados_dado_raw(self):
+        roman = {
+            "passport": 'ID#: WK9XA-LKM0Q\nNATION: United Federation\nNAME: Dolanski, Roman\nDOB: 1933.01.01\nSEX: M\nISS: Shingleton\nEXP: 1983.05.12',
+            "grant_of_asylum": 'NAME: Dolanski, Roman\nNATION: United Federation\nID#: Y3MNC-TPWQ2\nDOB: 1933.01.01\nHEIGHT: 176cm\nWEIGHT: 71kg\nEXP: 1983.09.20'
+        }
+        entrante = Entrante(roman, [lambda: None])
+        papeis = entrante.constroi_papeis(roman)
+        for p in papeis:
+            assert p in entrante.papeis
 
 
 class TestPapel:
@@ -22,4 +40,34 @@ class TestPapel:
             'exp': '1983.03.15'
         }
         assert papel.nome == esperado_nome
-        assert papel.items == esperado_items
+        assert papel.itens == esperado_items
+
+
+class TestRegras:
+    def test_regra_consistência_documentos_id_diferente(self):
+        roman = {
+            "passport": 'ID#: WK9XA-LKM0Q\nNATION: United Federation\nNAME: Dolanski, Roman\nDOB: 1933.01.01\nSEX: M\nISS: Shingleton\nEXP: 1983.05.12',
+            "grant_of_asylum": 'NAME: Dolanski, Roman\nNATION: United Federation\nID#: Y3MNC-TPWQ2\nDOB: 1933.01.01\nHEIGHT: 176cm\nWEIGHT: 71kg\nEXP: 1983.09.20'
+        }
+        entrante = Entrante(roman, [regra_consistência_documentos])
+        entrante.submete_papeis_regras()
+        assert entrante.status == ['Detainment: ID number mismatch.']
+
+    def test_regra_validade_documentos_passaporte_vencido(self):
+        josef = {"passport": 'ID#: GC07D-FU8AR\nNATION: Arstotzka\nNAME: Costanza, Josef\nDOB: 1933.11.28\nSEX: M\nISS: East Grestin\nEXP: 1930.03.15'}
+        entrante = Entrante(josef, [regra_valida_data])
+        entrante.submete_papeis_regras()
+        assert entrante.status == ['Entry denied: passport expired.']
+
+    def test_regra_validade_documentos_passaporte_e_asilo_vencidos(self):
+        roman = {
+            "passport": 'ID#: Y3MNC-TPWQ2\nNATION: United Federation\nNAME: Dolanski, Roman\nDOB: 1933.01.01\nSEX: M\nISS: Shingleton\nEXP: 1912.05.12',
+            "grant_of_asylum": 'NAME: Dolanski, Roman\nNATION: United Federation\nID#: Y3MNC-TPWQ2\nDOB: 1933.01.01\nHEIGHT: 176cm\nWEIGHT: 71kg\nEXP: 1900.09.20'
+        }
+        entrante = Entrante(roman, [regra_valida_data])
+        entrante.submete_papeis_regras()
+        assert entrante.status == ['Entry denied: passport expired.', 'Entry denied: grant_of_asylum expired.']
+
+
+class TestBoletim:
+    ...
